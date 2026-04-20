73c4ec3fb4e8ffc9795b8ae4c511acf1be93caa2
legaladdressby <legaladdress.by0@gmail.com>
2026-04-20 12:43:40 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,18 +3,17 @@ import telebot
 from flask import Flask
 from threading import Thread
 
-# Замените 'YOUR_BOT_TOKEN' на реальный токен вашего бота
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+# 1. ЗДЕСЬ ВАШ ТОКЕН
+BOT_TOKEN = '8737521251:AAGpW6bvG1vZRTx279pjD8KnWqfOvtx4HE0'
 
 bot = telebot.TeleBot(BOT_TOKEN)
 app = Flask('')
 
-# Здесь будет вся логика вашего бота (обработчики команд и т.д.)
+# Здесь будет вся логика вашего бота
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.reply_to(message, 'Привет! Я работаю на Koyeb!')
 
-# Этот обработчик нужен, чтобы бот отвечал на текстовые сообщения
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
